be9a4479da1b91d61a9d88d5a26804d3d3a9df75
naima-shk <naimashaikh550@gmail.com>
2021-07-06 22:22:14 +0500

    'todo-list-in-progress'

--- app.js
+++ app.js
@@ -1,55 +1,40 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+/* pseudocode 
+1- call the main function to get the data enter by user.
+2- if the value is empty then we use the conditions to check.
+3-
+*/
+function addMore(){
+  document.getElementById('error').innerHTML="";
+  let name= document.getElementById('name').value;
+  if(name == ''){
+    document.getElementById('error').innerHTML='please enter value';
+    //console.log(name);
+  }else{
+    let box= document.getElementById('box');
+    let li =document.createElement('li');
+     li.textContent=name;
+     let a = document.createElement('a');
+     a.textContent='x';
+    a.href ='javaScript:void(0)';
+    a.className='remove';
+    li.appendChild(a);
+     let pos = box.firstElementChild;
+     if(pos == null){
+       box.appendChild(li);
+     } else{
+       box.insertBefore(li,pos);
+     }
+     //let a = document.createElement('a');
+     //a.textContent='x';
+     //a.href ='javaScript';
+     
+  }
+  document.getElementById('name').value;
+
 }
-
-// Click on a close button to hide the current list item
-/*var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}*/
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-}
+let btn= document.querySelector('ul');
+btn.addEventListener('click', function (e){
+  let box= document.getElementById('box');
+  let li= e.target.parentNode;
+  box.removeChild(li);
+})
