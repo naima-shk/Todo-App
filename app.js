17be7a43ad3460223a503c37ba40824aacd82605
naima-shk <naimashaikh550@gmail.com>
2021-08-06 16:21:05 +0500

    'incorporated-second-mentor-meetup-feedback'

--- app.js
+++ app.js
@@ -3,40 +3,61 @@
 2- if the value is empty then we use the conditions to check.
 3- 
 */
-function addMore(){
-  document.getElementById('error').innerHTML="";
-  let name= document.getElementById('name').value;
-  if(name == ''){
-    document.getElementById('error').innerHTML='please enter value';
-    //console.log(name);
-  }else{
-    let box= document.getElementById('box');
-    let li =document.createElement('li');
-     li.textContent=name;
-     let a = document.createElement('a');
-     a.textContent='x';
-     //it does not redirect when we click on button
-    a.href ='javaScript:void(0)';
-    a.className='remove';
-    li.appendChild(a);
-    //check the position of the text
-     let pos = box.firstElementChild;
-     if(pos == null){
-       box.appendChild(li);
-     } else{
-       box.insertBefore(li,pos);
-     }
-     //let a = document.createElement('a');
-     //a.textContent='x';
-     //a.href ='javaScript';
-     
-  }
-  document.getElementById('name').value;
+// first we are setting  countdown in heading..
+$ = function(val) {
+    result = document.querySelector(val);
+    return result;
+};
 
+all = function(val) {
+    result = document.querySelectorAll(val);
+    return result;
+};
+
+function countList() {
+    let count = 0;
+    count = all("#box li").length;
+    console.log(count);
+    return count;
 }
-let btn= document.querySelector('ul');
-btn.addEventListener('click', function (e){
-  let box= document.getElementById('box');
-  let li= e.target.parentNode;
-  box.removeChild(li);
-})
+
+function addMore() {
+    document.getElementById("error").innerHTML = "";
+    let name = document.getElementById("name").value;
+    if (name == "") {
+        document.getElementById("error").innerHTML = "please enter value";
+        //console.log(name);
+    } else {
+        let box = document.getElementById("box");
+        let li = document.createElement("li");
+        li.textContent = name;
+        let a = document.createElement("a");
+        a.textContent = "x";
+        //it does not redirect when we click on button
+        a.href = "javaScript:void(0)";
+        a.className = "remove";
+        li.appendChild(a);
+        //check the position of the text
+        let pos = box.firstElementChild;
+        if (pos == null) {
+            box.appendChild(li);
+        } else {
+            box.insertBefore(li, pos);
+        }
+        //let a = document.createElement('a');
+        //a.textContent='x';
+        //a.href ='javaScript';
+    }
+    document.getElementById("name").value;
+}
+let btn = document.querySelector("ul");
+btn.addEventListener("click", function(e) {
+    let box = document.getElementById("box");
+    let li = e.target.parentNode;
+    box.removeChild(li);
+    if (countList() >= 1) {
+        $("#count").innerHTML = countList();
+    } else {
+        $("#count").innerHTML = "";
+    }
+});
